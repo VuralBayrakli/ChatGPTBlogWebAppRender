6be5c8b9fb633336b1aaed61479ea03e602c63d4
VuralBayrakli <vuralll360@gmail.com>
2023-06-07 18:00:17 +0300

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,3 @@
-set -e
-
-# Docker Compose dosyasının konumunu alın
-DOCKER_COMPOSE_FILE="/docker-compose.yml"
-
-# Docker Compose'yi başlatın
-docker-compose -f "$DOCKER_COMPOSE_FILE" up
+docker-compose build
+docker-compose up -d
 gunicorn grup_14.wsgi:application --bind 0.0.0.0:8000
