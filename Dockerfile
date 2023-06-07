f52c45ce948da07eb58f9d3341dfa0b9e9dbc9ab
VuralBayrakli <vuralll360@gmail.com>
2023-06-07 17:53:38 +0300

    commit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.9-alpine3.13
+FROM docker:20.10.5-dind
 
 
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
-docker-compose -f /docker-compose.yml build 
-docker-compose /docker-compose.yml up -d
+set -e
+
+# Docker Compose dosyasının konumunu alın
+DOCKER_COMPOSE_FILE="/docker-compose.yml"
+
+# Docker Compose'yi başlatın
+docker-compose -f "$DOCKER_COMPOSE_FILE" up
 gunicorn grup_14.wsgi:application --bind 0.0.0.0:8000
